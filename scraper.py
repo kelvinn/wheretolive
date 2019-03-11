ca0410e3e4dc358a5a24c32643c2440902ad200b
Kelvin Nicholson <kelvin@kelvinism.com>
2019-03-11 22:46:16 +1100

    Do print statements work instead of logging statements

--- scraper.py
+++ scraper.py
@@ -86,7 +86,7 @@ def scrape(num_pages=20):
 
 def enrich_records(records):
     enriched = []
-    logging.info(f'Starting enrichment...')
+    print(f'Starting enrichment...')
     for record in records:
         address, price, url = record
         p = session.query(RealEstate).filter_by(address=address).count()
