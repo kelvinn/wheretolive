ba12690fd91aaecb2c3eb0a393dd412d7a97066d
Kelvin Nicholson <kelvin@kelvinism.com>
2019-05-14 20:49:09 +1000

    Scrape 30 instead of 20

--- scraper.py
+++ scraper.py
@@ -19,7 +19,8 @@ session = Session()
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 
-def scrape(num_pages=20):
+
+def scrape(num_pages=30):
     # Set variables.
     addresses = []
     urls = []
