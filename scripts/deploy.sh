d875bfae5f4c37f635bc309847b917c97b6218ed
Kelvin Nicholson <kelvin@kelvinism.com>
2023-10-08 14:57:30 +1100

    Test out this deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,11 @@ TAG="0.0.$GITHUB_RUN_NUMBER" # Initially set this to "0.0.1", and then update to
 MACHINE_ID=$MACHINE_ID  # Initially hard code this to your machine ID for the first deploy, e.g. "e2865c22a34548", and then update to use whatever env var is in Github Actions
 APP_NAME="wheretolive-a32cd"
 
-docker build -t registry.fly.io/$APP_NAME:deployment-$TAG . --platform linux/amd64
-docker push registry.fly.io/$APP_NAME:deployment-$TAG
-fly machine update $MACHINE_ID --yes --image registry.fly.io/$APP_NAME:deployment-$TAG --schedule=daily --metadata fly_process_group=worker
+fly deploy
+
+# docker build -t registry.fly.io/$APP_NAME:deployment-$TAG . --platform linux/amd64
+# docker push registry.fly.io/$APP_NAME:deployment-$TAG
+# fly machine update $MACHINE_ID --yes --image registry.fly.io/$APP_NAME:deployment-$TAG --schedule=daily --metadata fly_process_group=worker
+
+MACHINE_ID=$(fly machine list --json | jq -r -c '.[] | select(.config | .metadata | .fly_process_group | contains("worker")) | .id')
+ly machine update $MACHINE_ID --yes --schedule=daily --metadata fly_process_group=worker
