42c5e0d0aaecfa7c4f64a0845047aa011efcbc13
Kelvin Nicholson <kelvin@kelvinism.com>
2023-10-15 09:21:26 +1100

    Make sure just a single worker is running

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,12 @@ set -e
 # See README.md for how to get these values
 
 TAG="0.0.$GITHUB_RUN_NUMBER" # Initially set this to "0.0.1", and then update to come from Github Actions
-MACHINE_ID=$MACHINE_ID  # Initially hard code this to your machine ID for the first deploy, e.g. "e2865c22a34548", and then update to use whatever env var is in Github Actions
+# MACHINE_ID=$MACHINE_ID  # Initially hard code this to your machine ID for the first deploy, e.g. "e2865c22a34548", and then update to use whatever env var is in Github Actions
 APP_NAME="wheretolive-a32cd"
 
-fly deploy
+fly deploy --wait-timeout 240
+fly scale count 1 --yes --process-group worker
+
 
 # docker build -t registry.fly.io/$APP_NAME:deployment-$TAG . --platform linux/amd64
 # docker push registry.fly.io/$APP_NAME:deployment-$TAG
